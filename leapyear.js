2af19b342b800c6e0fd5a0ba851bc76e34a6412e
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-06-02 22:40:35 -0400

    console.log function to see if it worked

--- leapyear.js
+++ leapyear.js
@@ -4,4 +4,8 @@ return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 
 };
 
+console.log(leapYear(2000));
+console.log(leapYear(2019));
+console.log(leapYear(2020));
+console.log(leapYear(2023));
 
